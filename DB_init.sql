05b2f5e886039e5f785e56e84862c14ca6b44e62
Kyle <baolegend9@gmail.com>
2023-11-26 13:45:21 +0700

    fixed database init
    added content for db

--- DB_init.sql
+++ DB_init.sql
@@ -2,4 +2,9 @@
  `id` int(11) NOT NULL AUTO_INCREMENT,
  `title` varchar(255) NOT NULL,
  PRIMARY KEY (`id`)
+INSERT INTO `navpages` (`id`, `title`) VALUES (NULL, 'Homepage');
+INSERT INTO `navpages` (`id`, `title`) VALUES (NULL, 'Categories');
+INSERT INTO `navpages` (`id`, `title`) VALUES (NULL, 'News');
+INSERT INTO `navpages` (`id`, `title`) VALUES (NULL, 'Contacts');
+INSERT INTO `navpages` (`id`, `title`) VALUES (NULL, 'Blogs');
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
